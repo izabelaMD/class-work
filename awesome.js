177d7017c834c2e5484abb97870f1c3450a12ede
izabelaMD <buckyfong@gmail.com>
2017-06-02 11:10:39 -0700

    change post title colors and home button

--- awesome.js
+++ awesome.js
@@ -28,18 +28,18 @@ const app = new Vue({
 
 			new Post(
 				'Looking for a Fellow Christian Lover?',
-				'https://vuejs.org',
+				'www.christianmingle.com',
 				'Christian Mingle Official',
 				'https://static.christianmingle.com/images/logo_facebook.png',
 				'Sign up for our free 30 day trial on Christian Mingle! Just use this promo code: CHRISTLOVE and find us at www.christianmingle.com!'
 				),
 
 			new Post(
-				'T',
+				'Bless you',
 				'https://google.com',
-				'Tim Lincecum',
-				'http://m.mlb.com/assets/images/9/1/8/98434918/cuts/Lincecum_1280_8qa6pl5x_2bjowjfa.jpg',
-				''
+				'violin4lyfe',
+				'https://sacwaldorf.org/wp-content/uploads/2016/01/Sutter-Nathan-150x150.jpg',
+				'Bless you all, my brothers and sisters.'
 				),
 
 			new Post(
@@ -59,11 +59,11 @@ const app = new Vue({
 				),
 				
 			new Post(
-				'S',
-				'https://google.com',
-				'Tim Linceum Is Life',
+				'Just joined a vampire dating site!!',
+				'https://www.vampiredatingsite.com',
+				'PhysicsVamp',
 				'https://sacwaldorf.org/wp-content/uploads/2016/01/Millard-John-1-150x150.jpg',
-				'check out my website: timlincecum.com for more of our favorite Jesus lookalike!'
+				'Looking for bloodthirsty singles in my area!'
 				),
 		]
 	},
